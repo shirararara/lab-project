fa8628c49eacd44e5cb374469fb439d82b9c8c09
flut22 <22.flut.22@gmail.com>
2026-04-14 21:41:57 +0300

    Update Class1.cs

--- RefactoringLab/RefactoringTool/Class1.cs
+++ RefactoringLab/RefactoringTool/Class1.cs
@@ -36,4 +36,9 @@ public class RefactoringService
     {
         return code;
     }
+       
+    public string ReplaceMagicNumber(string code, string number, string constantName)
+    {
+        return code;
+    }
 }
