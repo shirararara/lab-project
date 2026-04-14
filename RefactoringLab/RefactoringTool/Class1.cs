81155ab683ff8d557f55d7aa0f98681219a8ff89
flut22 <22.flut.22@gmail.com>
2026-04-14 21:41:21 +0300

    Update Class1.cs

--- RefactoringLab/RefactoringTool/Class1.cs
+++ RefactoringLab/RefactoringTool/Class1.cs
@@ -2,33 +2,8 @@ namespace RefactoringTool;
 
 public class RefactoringService
 {
-    public string RenameMethod(string code, string oldName, string newName)
+   public string ReplaceMagicNumber(string code, string number, string constantName)
     {
         return code;
-    }
-
-    public bool MethodExists(string code, string methodName)
-    {
-        return false;
-    }
-
-    public string AddParameter(string code, string methodName, string type, string paramName)
-    {
-        return code;
-    }
-
-    public string UpdateMethodCalls(string code, string methodName, string defaultValue)
-    {
-        return code;
-    }
-
-    public bool HasParameter(string code, string methodName, string paramName)
-    {
-        return false;
-    }
-
-    public bool IsValidParameterName(string name)
-    {
-        return false;
     }
 }
